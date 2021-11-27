965b67285c3d81644c64612149ac6e786eb0b98e
vladNEKvodan <90024139+vladNEKvodan@users.noreply.github.com>
2021-11-27 15:29:55 +0300

    Add files via upload

--- Работа на семинаре (11-12)/script.js
+++ Работа на семинаре (11-12)/script.js
@@ -1,46 +1,75 @@
-console.log('First');
-let gorod = {};
-gorod.name = "ГородN";
-gorod.population = "10.000.000";
-console.log('Name: ' + gorod.name + ', Population: ' + gorod.population);
-console.log('\n');
-
-
-
-console.log('Second');
-let gorad = {name: "ГородM", population: '1e6'};
-console.log('Name: ' + gorad.name + ', Population: ' + gorad.population);
-console.log('\n');
-
-
-
-console.log('Third');
-function getName() {
-    return this.name;
+console.log(`Fifth`)
+function Card(from, to){
+    this.from = from;
+    this.to = to;
+    this.show = function(){
+        alert(`from ${this.from} to ${this.to}`);
+    }
 }
-gorod.getName = getName;
-gorad.getName = getName;
-console.log(gorod.getName());
-console.log(gorad.getName());
-console.log('\n');
+let c1 = new Card("Екатеринбург", "Москва");
+c1.show();
 
 
+let red_card = new Card(12,5)
+red_card.show();
 
-console.log('Four');
-function exportStr() {
-    return `name=${this.name}\npopulation=${this.population}\n`;
+
+console.log(`Sixteen`)
+class Human{
+    constructor(name, age, height){
+        this.name =name,
+            this.age = age,
+            this.height = height
+    }
+
+    getInfo(){
+        return `${this.name}, ${this.age}, ${this.height}`
+    }
+
+    firstName(){
+        let descriptor = Object.getOwnPropertyDescriptor(this, 'name')
+        console.log(JSON.stringify(descriptor))
+    }
 }
-gorod.exportStr = exportStr;
-gorad.exportStr = exportStr;
-console.log(gorod.exportStr());
-console.log(gorad.exportStr());
-console.log('\n');
 
 
+console.log(`Seventeen`)
+humans =[
+    new Human('Коля',23,180),
+    new Human('Даша',19,170),
+    new Human('Ваня',18,192),
+    new Human('Петя',45,178),
+    new Human('Вася',34,197),
+    new Human('Джони',40,168),
+    new Human('Катя',37,160),
+    new Human('Петя',29,200),
+    new Human('Соня', 21,172),
+    new Human('Женя',25,175)
+]
 
-console.log('Five');
-function getObj () { return this }
-gorod.getCity = getObj;
-gorad.getCity = getObj;
-console.log('Name: ' + gorod.getCity().name + ', Population: ' + gorod.getCity().population);
-console.log('Name: ' + gorad.getCity().name + ', Population: ' + gorad.getCity().population);
+function sortByName(array) {
+    return array.sort(function comparator(num1, num2){ return (num1.name > num2.name) ? 1 : -1});
+}
+
+function sortByHeight(array) {
+    return array.sort(function comparator(num1, num2){return(num1.height < num2.height) ? 1 : -1});
+}
+
+console.log(humans);
+console.log(humans[3].getInfo());
+console.log(humans[7].firstname);
+console.log(sortByHeight(humans));
+
+dt1 = new Date(2045, 0, 1, 0, 0, 0);
+
+dt2 = Date.now() / 1000;
+
+function getDays(y, m) {
+    return new Date(y,m,0).getDate();
+}
+console.log(dt1);
+console.log(dt2 + ' s');
+console.log(getDays(2002, 01) + ' days');
+console.log(getDays(2102, 02) + ' days');
+console.log(getDays(2008, 02) + ' days');
+console.log(getDays(2023, 02) + ' days');
